2d3b9c00942e6a1822983b50545ff9ed2f03d915
TheMly <mlyrandom@gmail.com>
2020-05-06 00:17:34 +0100

    Created main page.

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import {HomeRoutingModule} from './home-routing.module';
+import {HeaderComponent} from '../shared/layout/header/header.component';
+
+@NgModule({
+  declarations: [HomeComponent, HeaderComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
