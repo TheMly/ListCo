f8de11a21aa787c16525d519b8a7150464179641
TheMly <mlyrandom@gmail.com>
2020-06-15 01:22:49 +0100

    Changed project structure in order to bundle the spring boot and the angular application. Updated project in order to integrate spring boot project with angular project and serve them together (still need to fix endpoint conflicts). Automated the project build using gradle and bundled the build result of the spring boot and the angular project (the client's jar is integrated on the server's jar)

---
+++ listco-server/src/main/java/com/server/listco/ListcoApplication.java
@@ -0,0 +1,13 @@
+package com.server.listco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListcoApplication.class, args);
+	}
+
+}
